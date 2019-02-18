32cebee363694821789b68f0b25205ca59e6908b
Jose Manuel Aceituno <jmac@data-ka.com>
2019-02-18 12:16:50 +0100

    Update api.py
    Modified to send direct messages.
    if you are on version 3.6.0, then you need:
    
    1. Add 2 methods to api.py file:
       [do-n-khanh@79772c9#diff-ea5dd38a4efd9ff36c96e04ab0597cfb](https://github.com/do-n-khanh/tweepy/commit/79772c976c64830149095f087c16c181912466ba#diff-ea5dd38a4efd9ff36c96e04ab0597cfb)

--- tweepy/api.py
+++ tweepy/api.py
@@ -13,7 +13,7 @@ from tweepy.binder import bind_api
 from tweepy.error import TweepError
 from tweepy.parsers import ModelParser, Parser
 from tweepy.utils import list_to_csv
-
+import json
 
 class API(object):
     """Twitter API"""
@@ -457,6 +457,17 @@ class API(object):
             allowed_param=['user', 'screen_name', 'user_id', 'text'],
             require_auth=True
         )
+    
+    def send_direct_message_new(self, messageobject):
+        """ :reference: https://developer.twitter.com/en/docs/direct-messages/sending-and-receiving/api-reference/new-event.html
+        """
+        headers, post_data = API._buildmessageobject(messageobject)
+        return bind_api(
+            api=self,
+            path = '/direct_messages/events/new.json',
+            method='POST',
+            require_auth=True
+        )(self, post_data=post_data, headers=headers)
 
     @property
     def destroy_direct_message(self):
@@ -1380,6 +1391,18 @@ class API(object):
         # build headers
         headers = {
             'Content-Type': 'multipart/form-data; boundary=Tw3ePy',
+            'Content-Length': str(len(body))
+        }
+
+        return headers, body
+    
+    """ Internal use only """
+    @staticmethod
+    def _buildmessageobject(messageobject):
+        body = json.dumps(messageobject)
+        # build headers
+        headers = {
+            'Content-Type': 'application/json',
             'Content-Length': str(len(body))
         }
 
